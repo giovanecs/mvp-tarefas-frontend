20aea3c21e996eca1beecdee1ac98a3d1285b17c
Giovane Silva <4dmatyjo@anonaddy.me>
2024-07-12 19:39:04 -0400

    refatoração da aplicação

--- scripts.js
+++ scripts.js
@@ -1,249 +1,188 @@
-const getTarefas = async () => {
-  let url = 'http://127.0.0.1:5000/tarefas';
-  fetch(url, {
-    method: 'get',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.tarefas.forEach(item => insertList(item.id, item.descricao, item.data_inicio, item.data_conclusao))
+const apiBaseUrl = 'http://127.0.0.1:5000';
+
+$(document).ready(function () {
+    const table = initializeDataTable();
+
+    getTarefas(table);
+
+    $('#addTaskForm').submit(function (event) {
+        event.preventDefault();
+        const description = $('#taskDescription').val();
+        addTask(description, table);
+    });
+});
+
+const getTarefas = async (table) => {
+    let url = `${apiBaseUrl}/tarefas`;
+    fetch(url, {
+        method: 'get',
     })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+        .then((response) => response.json())
+        .then((data) => {
+            table.clear();
+            data.tarefas.forEach(item => insertList(item.id, item.descricao, item.data_inicio, item.data_conclusao, table));
+            table.draw();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
-const getTarefasPendentes = async () => {
-  try {
-    const url = 'http://127.0.0.1:5000/tarefas/pendentes';
-    const response = await fetch(url);
-    const data = await response.json();
+const insertList = (id, descricao, data_inicio, data_conclusao, table) => {
+    var item = [id, descricao, data_inicio || 'Não iniciado', data_conclusao || 'Não concluído', createTaskActions(id)];
+    table.row.add(item);
 
-    const listaOrigem = document.getElementById('tarefas-pendentes');
-    const listaDestino = document.getElementById('tarefas-iniciadas');
+    // Atualizar as listas de tarefas agrupadas
+    const status = data_conclusao ? 'completedTasks' : (data_inicio ? 'startedTasks' : 'pendingTasks');
+    $(`#${status}`).append(`<p class="kanban-item">${descricao}</p>`);
 
-    data.tarefas.forEach((item) => {
-      const novoElemento = criarElementoComBotao(item.id, item.descricao, listaOrigem, listaDestino, "iniciar" );
-      listaOrigem.appendChild(novoElemento);
-    });
-  } catch (error) {
-    console.error('Erro ao carregar tarefas:', error);
-  }
+
+    $('#taskDescription').val('');
+    $('#taskDate').val('');
 }
 
-const getTarefasIniciadas = async () => {
-  try {
-    const url = 'http://127.0.0.1:5000/tarefas/iniciadas';
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const listaOrigem = document.getElementById('tarefas-iniciadas');
-    const listaDestino = document.getElementById('tarefas-concluidas');
-
-    data.tarefas.forEach((item) => {
-      const novoElemento = criarElementoComBotao(item.id, item.descricao, listaOrigem, listaDestino, "concluir");
-      listaOrigem.appendChild(novoElemento);
-    });
-  } catch (error) {
-    console.error('Erro ao carregar tarefas:', error);
-  }
-} 
-
-const getTarefasConcluidas = async () => {
-  try {
-    const url = 'http://127.0.0.1:5000/tarefas/concluidas';
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const lista = document.getElementById('tarefas-concluidas');
-  
-    data.tarefas.forEach((item) => {
-      const novoElemento = criarElementoSemBotao(item.descricao);
-      lista.appendChild(novoElemento);
-    });
-  } catch (error) {
-    console.error('Erro ao carregar tarefas:', error);
-  }
+const createTaskActions = (taskId) => {
+    return `
+        <button class="btn btn-success" onclick="startTask(${taskId})" alt="Iniciar tarefa"> <i class="fas fa-play"></i> </button>
+        <button class="btn btn-warning" onclick="completeTask(${taskId})" alt="Concluir tarefa"> <i class="fas fa-check"></i> </button>
+        <button class="btn btn-danger" onclick="deleteTask(${taskId})" alt="Deletar tarefa"> <i class="fas fa-trash"></i> </button>
+    `;
 }
 
-getTarefas()
-getTarefasPendentes()
-getTarefasIniciadas()
-getTarefasConcluidas()
+const addTask = async (description, table) => {
+    const formData = new FormData();
+    formData.append('descricao', description);
 
+    let url = `${apiBaseUrl}/tarefa/add`;
+    fetch(url, {
+        method: 'post',
+        body: formData
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return response.json().then((errorData) => {
+                    throw new Error(`${errorData.message}`);
+                });
+            }
+        })
+        .then((data) => {
+            insertList(data.id, data.descricao, data.data_inicio, data.data_conclusao, table);
+            table.draw();
+            alert('Tarefa adicionada com sucesso!!!');
+        })
+        .catch((error) => {
+            console.error(error);
+            alert(error.message);
+        });
+}
 
-const postTarefa = async (inputTarefa) => {
-  const formData = new FormData();
-  formData.append('descricao', inputTarefa);
-  
-  let url = 'http://127.0.0.1:5000/tarefa/add';
-  fetch(url, {
-    method: 'post',
-    body: formData
-  })
-  .then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      return response.json().then((errorData) => {
-        throw new Error(`${errorData.message}`);
-      });
-    }
-  })
-  .then((data) => {
-    insertList(data.id, data.descricao, data.data_inicio, data.data_conclusao);
-    appendAlert('Tarefa adicionada com sucesso!!!', 'success');
-  })
-  .catch((error) => {
-    console.error(error); 
-    appendAlert(error.message, 'danger');
-  });
+const startTask = async (taskId) => {
+    await atualizarTarefa(taskId, 'iniciar');
+    // Atualizar a lista de tarefas iniciadas
+    const table = $('#allTasks').DataTable();
+    const rowData = table.row($(`button[onclick="startTask(${taskId})"]`).closest('tr')).data();
+    const descricao = rowData[1];
+}
+
+const completeTask = async (taskId) => {
+    await atualizarTarefa(taskId, 'concluir');
+    // Atualizar a lista de tarefas concluídas
+    const table = $('#allTasks').DataTable();
+    const rowData = table.row($(`button[onclick="completeTask(${taskId})"]`).closest('tr')).data();
+    const descricao = rowData[1];
+}
+
+const deleteTask = async (taskId) => {
+    let url = `${apiBaseUrl}/tarefa?id=${taskId}`;
+    fetch(url, {
+        method: 'delete'
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                const table = $('#allTasks').DataTable();
+                table.row($(`button[onclick="deleteTask(${taskId})"]`).closest('tr')).remove().draw();
+                alert('Tarefa removida com sucesso!!!');
+            } else {
+                return response.json().then((errorData) => {
+                    throw new Error(`${errorData.message}`);
+                });
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert(error.message);
+        });
 }
 
 const atualizarTarefa = async (id, operacao) => {
-  const formData = new FormData();
-  formData.append('id', id);
-  let url = '';
-  
-  if(operacao == 'iniciar'){
-    url = 'http://127.0.0.1:5000/tarefa/iniciar';
-  }else{
-    url = 'http://127.0.0.1:5000/tarefa/concluir';
-  }
+    const formData = new FormData();
+    formData.append('id', id);
+    let url = '';
 
-  fetch(url, {
-    method: 'post',
-    body: formData
-  })
-  .then((response) => {
-    if (response.status === 200) {
-      return response.json();
+    if (operacao == 'iniciar') {
+        url = `${apiBaseUrl}/tarefa/iniciar`;
     } else {
-      return response.json().then((errorData) => {
-        throw new Error(`${errorData.message}`);
-      });
+        url = `${apiBaseUrl}/tarefa/concluir`;
     }
-  })
-  .then((data) => {
-    insertList(data.id, data.descricao, data.data_inicio, data.data_conclusao);
-  })
-  .catch((error) => {
-    console.error(error); 
-    appendAlert(error.message, 'danger');
-  });
+
+    fetch(url, {
+        method: 'post',
+        body: formData
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return response.json().then((errorData) => {
+                    throw new Error(`${errorData.message}`);
+                });
+            }
+        })
+        .then((data) => {
+            const table = $('#allTasks').DataTable();
+            table.row($(`button[onclick="${operacao}Task(${id})"]`).closest('tr')).remove().draw();
+            insertList(data.id, data.descricao, data.data_inicio, data.data_conclusao, table);
+            table.draw();
+            alert(`Tarefa ${operacao}ada com sucesso!!!`);
+        })
+        .catch((error) => {
+            console.error(error);
+            alert(error.message);
+        });
+    
+        getTarefas();
 }
 
-
-const insertButton = (parent) => {
-  let button = document.createElement("button");
-  let txt = document.createTextNode("\u00D7");
-  button.className = "btn btn-secundary close";
-  button.appendChild(txt);
-  parent.appendChild(button);
-}
-
-
-const removeElement = () => {
-  let close = document.getElementsByClassName("close");
-
-  let i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement.parentElement;
-      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-      if (confirm("Você tem certeza?")) {
-        div.remove()
-        deleteItem(nomeItem)
-        alert("Removido!")
-      }
-    }
-  }
-}
-
-const deleteItem = (item) => {
-  console.log(item)
-  let url = 'http://127.0.0.1:5000/tarefa?id=' + item;
-  fetch(url, {
-    method: 'delete'
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
+const initializeDataTable = () => {
+    return $('#allTasks').DataTable({
+        "pageLength": 5,
+        "searching": false,
+        "lengthChange": false,
+        "language": {
+            "decimal": "",
+            "emptyTable": "Nenhuma tarefa disponível",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ tarefas",
+            "infoEmpty": "Mostrando 0 a 0 de 0 tarefas",
+            "infoFiltered": "(filtrado de _MAX_ tarefas no total)",
+            "infoPostFix": "",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ tarefas",
+            "loadingRecords": "Carregando...",
+            "processing": "Processando...",
+            "search": "Pesquisar:",
+            "zeroRecords": "Nenhuma tarefa encontrada",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": ativar para ordenar a coluna em ordem crescente",
+                "sortDescending": ": ativar para ordenar a coluna em ordem decrescente"
+            }
+        }
     });
 }
-
-const novaTarefa = () => {
-  let inputTarefa = document.getElementById("inputTarefa").value;
-
-  if (inputTarefa === '') {
-    appendAlert('Escreva sua nova tarefa!!!', 'warning')
-  } else {
-    postTarefa(inputTarefa)
-  }
-}
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para inserir items na lista apresentada
-  --------------------------------------------------------------------------------------
-*/
-const insertList = (id, descricao, data_inicio, data_conclusao) => {
-  var item = [id, descricao, data_inicio, data_conclusao]
-  var table = document.getElementById('myTable');
-  var row = table.insertRow();
-
-  for (var i = 0; i < item.length; i++) {
-    var cel = row.insertCell(i);
-    cel.textContent = item[i];
-  }
-  insertButton(row.insertCell(-1))
-  document.getElementById("inputTarefa").value = "";
-  
-  removeElement()
-}
-
-const alertPlaceholder = document.getElementById('dynamic-alert')
-
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-
-function moverElementoDeLista(elementoParaMover, listaOrigem, listaDestino) {
-  listaOrigem.removeChild(elementoParaMover);
-  listaDestino.appendChild(elementoParaMover);
-}
-
-function criarElementoComBotao(id, descricao, listaOrigem, listaDestino, operacao) {
-  const liElement = document.createElement('li');
-  liElement.className = "list-group-item";
-  liElement.textContent = descricao;
-
-  const buttonElement = document.createElement('button');
-  buttonElement.className= "btn btn-secundary"
-  buttonElement.textContent = operacao;
-  buttonElement.addEventListener('click', () => {
-    moverElementoDeLista(liElement, listaOrigem, listaDestino);
-    atualizarTarefa(id, operacao)
-  });
-
-  liElement.appendChild(buttonElement);
-
-  return liElement;
-}
-
-function criarElementoSemBotao(descricao) {
-  let liElement = document.createElement('li');
-  liElement.className = "list-group-item";
-  liElement.textContent = descricao;
-  return liElement;
-}
-
-
